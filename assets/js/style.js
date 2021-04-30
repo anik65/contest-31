b9d4e41a1df8cb19f601f5ee35622bed69156993
anik65 <www.hyper152@gmail.com>
2021-04-30 18:22:16 +0600

    Contest-31
    done

---
+++ assets/js/style.js
@@ -0,0 +1,57 @@
+
+//  sticky nav
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+        $("nav").addClass("fixed-top");
+    } else {
+        $("nav").removeClass("fixed-top");
+    }
+});
+ 
+ 
+$('.popup-with-zoom-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+});
+
+$('.B-1').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+});
+$('.B-2').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+});
